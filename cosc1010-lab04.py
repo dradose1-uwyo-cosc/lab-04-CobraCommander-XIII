b1c529b0ebff57de93383f8e1c0d6b0a87d86853
CobraCommander-XIII <rmcguir4@uwyo.edu>
2024-10-02 21:43:50 +0000

    edited

--- cosc1010-lab04.py
+++ cosc1010-lab04.py
@@ -107,15 +107,15 @@ print(f"Average high temperature: {max_sum / len(max_temps)} degrees\nAverage lo
 
 #I initially used the following solution before receiving assistance, but this uses two loops instead of one:
 
-#max_sum = 0
-#for max in max_temps:
-    #max_sum = max + max_sum
-#print(f"Average high temperature: {max_sum / len(max_temps)} degrees")
+    #max_sum = 0
+    #for max in max_temps:
+        #max_sum = max + max_sum
+    #print(f"Average high temperature: {max_sum / len(max_temps)} degrees")
 
-#min_sum = 0
-#for min in min_temps:
-    #min_sum = min + min_sum
-#print(f"Average low temperature: {min_sum / len(min_temps)} degrees")
+    #min_sum = 0
+    #for min in min_temps:
+        #min_sum = min + min_sum
+    #print(f"Average low temperature: {min_sum / len(min_temps)} degrees")
 
 # For the next part, determine the highest and lowest recorded temperatures.
 # HINT: You can find this information without using any logic or looping.
@@ -125,8 +125,8 @@ print(f"Lowest recorded temperature: {min(min_temps)} degrees")
 
 #Before receiving assistance with the simpler solution using the max and min functions, this was my solution:
 
-#max_temps.sort()
-#print(f"Highest recorded temperature: {max_temps[-1]} degrees")
+    #max_temps.sort()
+    #print(f"Highest recorded temperature: {max_temps[-1]} degrees")
 
-#min_temps.sort()
-#print(f"Lowest recorded temperature: {min_temps[0]} degrees")
+    #min_temps.sort()
+    #print(f"Lowest recorded temperature: {min_temps[0]} degrees")
